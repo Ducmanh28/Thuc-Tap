fb9529c1643b682de3664ed4c4393214f5bd8dea
Ducmanh28 <95476360+Ducmanh28@users.noreply.github.com>
2024-12-06 08:17:03 +0700

    cập nhật code

--- Linux/06. NetBox/Tools/Bot_Tele/config.py
+++ Linux/06. NetBox/Tools/Bot_Tele/config.py
@@ -2,10 +2,10 @@
 ADMIN_IDS = ['@ducmanh2873']
 
 # Enter the URL of NetBox
-URLNETBOX = "https://172.16.66.82"              
+URLNETBOX = "https://www.netboxlab.local"              
 
 # Enter the NetBox Token                    
-TOKENNETBOX = "aa8f29998abd6a63f476a2328ce2a629a506b579	"
+TOKENNETBOX = "aa8f29998abd6a63f476a2328ce2a629a506b579"
 
 # Enter the Token Bot Telegram
 TOKENTELEGRAM = "7660744016:AAHiWWvkRbVxtW40L-VHrE0IEDRXW9rMieU"
